5482d7156350debb632d05284120b982573ad796
Alex Pyzhianov <alex.pyzhianov@gmail.com>
2016-04-13 19:52:49 +0300

    Fix React dependancy, add tslint.json

--- victory.d.ts
+++ victory.d.ts
@@ -3,19 +3,19 @@
 // Definitions by: Alex Pyzhianov <https://github.com/SereznoKot>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-import * as React from 'react';
-
 declare namespace Victory {
+  import React = __React;
+
   interface VictoryLineProps {
-    height: number,
-    interpolation: string,
-    padding: number,
-    samples: number,
-    scale: string,
-    standalone: boolean,
-    width: number,
-    x: string,
-    y: string
+    height: number;
+    interpolation: string;
+    padding: number;
+    samples: number;
+    scale: string;
+    standalone: boolean;
+    width: number;
+    x: string;
+    y: string;
   }
 
   class VictoryLine extends React.Component<VictoryLineProps, any> {
