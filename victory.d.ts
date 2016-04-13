58177322551beb2203fab15e41da6fa359c31209
Alex Pyzhianov <alex.pyzhianov@gmail.com>
2016-04-13 21:16:26 +0300

    ImproveVictoryLine, add VictoryAnimation

--- victory.d.ts
+++ victory.d.ts
@@ -5,17 +5,68 @@
 
 declare namespace Victory {
   import React = __React;
+  /* VictoryAnimate */
+  interface VictoryAnimationProps {
+    // children: ???
+    duration?: number;
+    easing?: Easing;
+    delay?: number;
+    onEnd?: Function;
+    data?: any[]; // UNCLEAR
+  }
+
+  class VictoryAnimation extends React.Component<VictoryAnimationProps, any> {
+  }
+
+  /* VictoryLine */
+  interface Stack {
+    parent: any; // UNCLEAR
+    data: any; // UNCLEAR
+    labels: any; // UNCLEAR
+  }
+
+  interface EventHandler {
+    (event: Event, props: any[], index: number): void;
+  }
+
+  type Custom = any; // UNCLEAR
+  type Shape = any; // UNCLEAR
+
+  type Easing =
+      'back' | 'backIn' | 'backOut' | 'backInOut' | 'bounce' | 'bounceIn' |
+      'bounceOut' | 'bounceInOut' | 'circle' | 'circleIn' | 'circleOut' |
+      'circleInOut' | 'linear' | 'linearIn' | 'linearOut' | 'linearInOut' |
+      'cubic' | 'cubicIn' | 'cubicOut' | 'cubicInOut' | 'elastic' |
+      'elasticIn' | 'elasticOut' | 'elasticInOut' | 'exp' | 'expIn' |
+      'expOut' | 'expInOut' | 'poly' | 'polyIn' | 'polyOut' | 'polyInOut' |
+      'quad' | 'quadIn' | 'quadOut' | 'quadInOut' | 'sin' | 'sinIn' |
+      'sinOut' | 'sinInOut';
+
+  type Interpolation =
+      'basis' | 'basisClosed' | 'basisOpen' | 'bundle' |
+      'cardinal' | 'cardinalClosed' | 'cardinalOpen' |
+      'catmullRom' | 'catmullRomClosed' | 'catmullRomOpen' |
+      'linear' | 'linearClosed' | 'monotoneX' | 'monotoneY' |
+      'natural' | 'radial' | 'step' | 'stepAfter' | 'stepBefore';
+
+  type DataAccessor = string | string[] | ((data: any[]) => number) | Custom;
 
   interface VictoryLineProps {
-    height: number;
-    interpolation: string;
-    padding: number;
-    samples: number;
-    scale: string;
-    standalone: boolean;
-    width: number;
-    x: string;
-    y: string;
+    data?: any[];
+    x?: DataAccessor;
+    y?: DataAccessor;
+    domain?: Custom|Shape;
+    padding?: number|Shape;
+    width?: Custom;
+    height?: Custom;
+    samples?: Custom;
+    interpolation?: Interpolation;
+    scale?: Custom|Shape;
+    label?: any; // UNCLEAR
+    standalone?: boolean;
+    style?: Stack;
+    events?: Stack;
+    animate?: VictoryAnimationProps;
   }
 
   class VictoryLine extends React.Component<VictoryLineProps, any> {
